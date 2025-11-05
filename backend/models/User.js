fcd955bf04266aa07647481c2637a66756da50f5
gayatri <gayatrimshirur@gmail.com>
2025-11-05 18:08:50 +0530

    changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema(
       trim: true,
       maxlength: [50, 'Name cannot be more than 50 characters'],
     },
-    email: {}
+    email: {
       type: String,
       required: [true, 'Please provide an email'],
       unique: true,
